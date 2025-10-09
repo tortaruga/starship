eb55c093b5ba41217f8102e73a66c8bdaac91c0a
tortaruga <jojoway77@gmail.com>
2025-10-09 12:22:49 +0200

    add ship systems panel

--- app.js
+++ app.js
@@ -3,6 +3,94 @@ import shipSpacesData from "./shipSpacesData.js";
 import locationsData from "./locationsData.js";
 import missions from "./missions.js";
 
+const engineStates = [
+    {
+        state: "surprisingly good",
+        severity: 'good',
+    },
+
+    {
+        state: "smoother than expected",
+        severity: 'good',
+    },
+
+    {
+        state: "stable",
+        severity: 'good',
+    },
+
+    {
+        state: "unreasonably efficient",
+        severity: 'good',
+    },
+
+    {
+        state: "not bad all things considered",
+        severity: 'caution',
+    },
+    
+    {
+        state: "holding together with duct tape and hope",
+        severity: 'caution',
+    },
+
+    {
+        state: "grumpy but functional",
+        severity: 'caution',
+    },
+
+    {
+        state: "could be worse, probably will be",
+        severity: 'caution',
+    },
+
+    {
+        state: "just about to explode",
+        severity: 'critical',
+    },
+
+    {
+        state: "screaming in binary",
+        severity: 'critical',
+    },
+
+    {
+        state: "just about to explode",
+        severity: 'critical',
+    },
+
+    {
+        state: "brace for impact...",
+        severity: 'critical',
+    },
+
+    {
+        state: "overheated, currently frying bacon on it",
+        severity: 'critical',
+    },
+
+    {
+        state: "stuck in existential crisis",
+        severity: 'glitch',
+    },
+
+     {
+        state: "unknown protocol: 42-ERROR-OMEGA",
+        severity: 'glitch',
+    },
+
+     {
+        state: "Schrödinger’s thrust",
+        severity: 'glitch',
+    },
+
+     {
+        state: "[REDACTED]",
+        severity: 'glitch',
+    },
+];
+
+
 class ShipRoom {
     constructor({name, description, action}) {
         this.name = name;
@@ -49,12 +137,13 @@ const starship = {
     model: "Junkyard Cruiser",
     description: 'An ugly but lovable starship cobbled together from scrap, spare parts, and salvaged tech; unpredictable and unreliable, its systems fail randomly, and it blasts rap music at full volume during stealth missions. No one knows where the speakers are.',
     technical_stats: {
-        speed_mph: 11400,
+        speed_mph: 11400, 
         shield_strength: 55,
         fuel_level: 50,
         hull_integrity: 35, 
         crew_morale: 46,
-        engine_state: 'online', // offline // damaged
+        engine_state: 'online',
+        ammo_levels: 23,
     },
     crew: [
         new CrewMember(crewData.umeko),
@@ -144,9 +233,25 @@ const starship = {
         if (this.technical_stats.crew_morale <= 0) {
             console.log("Morale is so low the crew is basically depressed. Let's liven up the place")
         }
+    },
+    
+    calculateSpeed: function() {
+       const maxSpeed = 120000;
+       this.technical_stats.speed_mph = maxSpeed * (this.technical_stats.hull_integrity / 100) * (this.technical_stats.fuel_level / 100); 
+    },
+
+    setEngineState: function() {
+        const randomIndex = Math.floor(Math.random() * engineStates.length);
+        this.technical_stats.engine_state = engineStates[randomIndex].state;
+    },
+
+    init: function() {
+        this.calculateSpeed();
+        this.setEngineState();
     }
 }
 
+starship.init();
 
 const currentStatusOptions = [
   "Drifting near asteroid belt",
@@ -251,3 +356,58 @@ document.getElementById('back-to-crew-list').addEventListener('click', () => {
     document.getElementById('member-card').classList.add('hide');
     document.getElementById('member-message').textContent = ''; 
 })
+
+// engine state
+
+function setEngineState() {
+    const randomIndex = Math.floor(Math.random() * engineStates.length);
+    return engineStates[randomIndex].state;
+}
+
+document.querySelector('[data-label="ship-systems"]').addEventListener('click', () => {
+    starship.technical_stats.engine_state = setEngineState();
+});
+
+// ship systems
+
+starship.calculateSpeed();
+
+function populateStats() {
+    document.querySelector('.stats-speed').textContent = 'speed: ' + starship.technical_stats.speed_mph + ' mph';
+    document.querySelector('.stats-engine').textContent = 'engine state: ' + starship.technical_stats.engine_state;
+}
+
+populateStats();
+
+// visulizers
+function createBarVisualizer(value, element) {
+    const completeBars = Math.trunc(value / 10);
+    const incompleteBars = value % 10;
+
+    for (let i = 0; i < 10; i++) {
+        const span = document.createElement('span');
+        span.classList.add('bar');
+
+        if (i < completeBars) {
+            span.classList.add('complete');
+        }
+
+        if ( i === completeBars) {
+            span.style.background = `linear-gradient(to right, blueviolet ${incompleteBars * 10}%, black ${incompleteBars * 10}%)`;
+        }
+         
+        element.appendChild(span);
+    }
+}
+
+ 
+function createLevelVisualizer(value, element) {
+    document.querySelector(`.${element} .inner-level`).style.width = `${value}%`;
+}
+
+createBarVisualizer(starship.technical_stats.fuel_level, document.querySelector('.fuel-visualizer'));
+createBarVisualizer(starship.technical_stats.shield_strength, document.querySelector('.shields-visualizer'));
+createBarVisualizer(starship.technical_stats.hull_integrity, document.querySelector('.hull-visualizer'));
+
+createLevelVisualizer(starship.technical_stats.crew_morale, 'morale-visualizer');
+createLevelVisualizer(starship.technical_stats.ammo_levels, 'ammo-visualizer'); 
